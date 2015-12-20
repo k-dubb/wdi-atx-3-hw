0f0e4e586e8ed52c0c1583a90b3499477d09ec7a
Kelli Waiss <kelliwaiss@hotmail.com>
2015-12-20 10:36:14 -0600

    w02d03 hw

--- w02/d03/starter/deck_o_cards.js
+++ w02/d03/starter/deck_o_cards.js
@@ -1,22 +1,44 @@
 // 1. Make the function deck_o_cards assemble an array of cards using the provided suits and values arrays. 
 // Each card in the deck should be an object formatted as: {suit: 'hearts', value: 'A'}
 function deck_o_cards() {
-	var values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'];
-  	var suits = ['hearts', 'diamonds', 'clubs', 'spades'];
+    var values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'];
+    var suits = ['hearts', 'diamonds', 'clubs', 'spades'];
 
-	// Create the arrays for the deck, and shuffled deck
-	var cards = [];
-	// Make 52 card objects and store them in the "cards" array
+    // Make 52 card objects and store them in the "cards" array
+    var cards = [];
 
-	// Shuffle the Deck
+    for (var v = 0; v < values.length; v++) {
+        var pickValue = values[v];
 
-	// Pull the top card
+            for (var s = 0; s < suits.length; s++) {
+                var pickSuit = suits[s];
 
-	// Console log the results
+            var newCard = new Card(pickValue, pickSuit);
+
+            cards.push(newCard);
+        }
+    }   
+    
+    //console.log(cards.length);
+
+    function Card(value, suit) { 
+        this.values = value;
+        this.suits = suit;
+    }
+    // Shuffle the Deck
+    shuffle(cards);
+
+    // Pull the top card
+    // Console log the results
+    var topCard = cards[0];
+
+    console.log("The deck has " + cards.length + " cards.");
+    console.log("The top card is the " + topCard.suits + " of " + topCard.values);
 }
-
-// Fisher-Yates Shuffle 
-// http://stackoverflow.com/a/6274398
+       
+    // Fisher-Yates Shuffle 
+    // http://stackoverflow.com/a/6274398
+    // Shuffle the Deck
 function shuffle(array) {
     var counter = array.length, temp, index;
 
@@ -33,6 +55,6 @@ function shuffle(array) {
         array[counter] = array[index];
         array[index] = temp;
     }
-
     return array;
+
 }
