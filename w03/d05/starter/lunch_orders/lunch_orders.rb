14d74f602a42ac691fa34493a4f7dabbedc83a31
Kelli Waiss <kelliwaiss@hotmail.com>
2015-12-24 09:26:02 -0800

    starting lunch order hw

--- w03/d05/starter/lunch_orders/lunch_orders.rb
+++ w03/d05/starter/lunch_orders/lunch_orders.rb
@@ -1,1 +1,36 @@
+print "Name for order: "
+name = gets.chomp
+name.capitalize!
+
+print "What do you want to order?"
+item = gets.chomp
+
+puts "#{name} + wants to order: #{item}"
+
+# hash!
 orders = {}
+
+#store the order in the hash
+puts orders["name"]
+puts orders["item"]
+
+
+print "Add another item to the order?"
+more.downcase = gets.chomp    
+    #    while yes, repeat
+    while more == "yes"
+        print "Name for order: "
+        name = gets.chomp
+        name.capitalize!
+
+        print "What else do you want to order?"
+        item = gets.chomp
+
+        puts "#{name} + wants to order: #{item}"
+        #store the order in the hash
+        puts orders["name"]
+        puts orders["item"]
+    end
+
+#once done, print out all orders
+print "All orders: " + orders.each{ |x, y| puts + "#{x}: #{y}"}"
