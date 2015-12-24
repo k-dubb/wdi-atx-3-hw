02ff99df0b76d80b97f01e144787e13050924894
Kelli Waiss <kelliwaiss@hotmail.com>
2015-12-24 09:27:52 -0800

    working on converting with for loop

--- w03/d05/starter/roman/roman.js
+++ w03/d05/starter/roman/roman.js
@@ -1,5 +1,14 @@
-function romanize(number){
-  var matches = {
+var number = prompt("What number do you want to convert to roman numerals?");
+
+function romanize(number);
+
+
+function romanize(){
+
+    var roman;
+    var i;
+
+    var matches = {
     1: "I",
     2: "II",
     3: "III",
@@ -9,6 +18,24 @@ function romanize(number){
     7: "VII",
     8: "VIII",
     9: "IX",
-    10: "X"
-  }
+    10: "X",
+    50: "L",
+    100: "C",
+    500: "D",
+    1000: "M",
+    40: "XL",
+    90: "XC",
+    400: "CD",
+    900: "CM"
+    }
+
+//    for (matches[i]) {
+        for (i in matches) {
+        while (number >= matches[i]) {
+            roman += i;
+            number -= matches[i];
+        }
+    }
+    return roman;
+    console.log(roman);
 }
