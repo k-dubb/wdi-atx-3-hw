98e63a7b282723991bec38bd5d9b8329696964b7
Kelli Waiss <kelliwaiss@hotmail.com>
2015-12-24 11:52:14 -0800

    still need to fix while loop and print everything out at end

--- w03/d05/starter/lunch_orders/lunch_orders.rb
+++ w03/d05/starter/lunch_orders/lunch_orders.rb
@@ -5,19 +5,18 @@ name.capitalize!
 print "What do you want to order?"
 item = gets.chomp
 
-puts "#{name} + wants to order: #{item}"
+puts "#{name} wants to order: #{item}"
 
 # hash!
 orders = {}
 
 #store the order in the hash
-puts orders["name"]
-puts orders["item"]
-
+orders["name"] = "item"
 
 print "Add another item to the order?"
-more.downcase = gets.chomp    
-    #    while yes, repeat
+more = gets.chomp    
+more.downcase
+    #while yes, repeat
     while more == "yes"
         print "Name for order: "
         name = gets.chomp
@@ -26,11 +25,17 @@ more.downcase = gets.chomp
         print "What else do you want to order?"
         item = gets.chomp
 
-        puts "#{name} + wants to order: #{item}"
+        puts "#{name} wants to order: #{item}"
+
         #store the order in the hash
-        puts orders["name"]
-        puts orders["item"]
+        orders["name"] = "item"
+
+        print "Add another item to the order?"
+        more = gets.chomp    
+        more.downcase
     end
 
 #once done, print out all orders
-print "All orders: " + orders.each{ |x, y| puts + "#{x}: #{y}"}"
+print orders
+#print print "All orders: " orders{name: item}
+#print "All orders: " orders.each{ |name, item| puts #{name}: #{item}}"
