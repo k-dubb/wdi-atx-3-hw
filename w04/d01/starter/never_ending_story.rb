3d897b280af8cf074656c1323a8103bbe03aeca7
Kelli Waiss <kelliwaiss@hotmail.com>
2016-01-05 09:59:29 -0600

    final w4d01 hw

--- w04/d01/starter/never_ending_story.rb
+++ w04/d01/starter/never_ending_story.rb
@@ -5,3 +5,16 @@ story = [
   'The spaceship hits a wormhole and crashes in 1941 Roswell, NM',
   'Fry encounters his grandmother, then inadvertently sires his father'
 ]
+
+story.each do |line|
+    puts line
+    puts "Should we continue? (y/n)"
+    answer = gets.chomp   
+        if answer == "y"
+            story.push(line) 
+        elsif answer == "n"
+            puts "That's the end of the story!"
+        break
+        end
+end
+
