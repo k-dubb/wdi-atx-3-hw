646fa0d3ebb1d7f629af7a0acaa22901ab7606e1
Kelli Waiss <kelliwaiss@hotmail.com>
2016-01-17 10:38:03 -0600

    wk4d2 completed hw

--- w04/d02/starter/word.rb
+++ w04/d02/starter/word.rb
@@ -6,4 +6,27 @@ class Word
   def original_word
     return @original_word.downcase
   end
+
+  def piglatinize
+    vowels = ["a", "e", "i", "o", "u"]
+
+        if !vowels.include?(original_word[0])
+            counter = 0
+            while !vowels.include?(original_word[counter])
+                counter += 1
+            end
+       
+            return original_word[counter..-1] + original_word[0..(counter-1)] + "ay"
+       
+       else 
+
+            return original_word + "way"  
+    
+        end
+
+    end
+
 end
+newWord = Word.new("christmas")
+
+p newWord.piglatinize
