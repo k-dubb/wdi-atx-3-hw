1275c937640f342a01ce14779987c33d9ae99db9
Kelli Waiss <kelliwaiss@hotmail.com>
2016-01-18 23:17:11 -0600

    w03d05 hw

--- w03/d05/starter/lunch_orders/lunch_orders.rb
+++ w03/d05/starter/lunch_orders/lunch_orders.rb
@@ -1,41 +1,25 @@
-print "Name for order: "
-name = gets.chomp
-name.capitalize!
-
-print "What do you want to order?"
-item = gets.chomp
-
-puts "#{name} wants to order: #{item}"
-
-# hash!
 orders = {}
 
-#store the order in the hash
-orders["name"] = "item"
+while true 
 
-print "Add another item to the order?"
-more = gets.chomp    
-more.downcase
-    #while yes, repeat
-    while more == "yes"
-        print "Name for order: "
-        name = gets.chomp
-        name.capitalize!
+    print "Name for order: "
+    name = gets.chomp
+    name.capitalize!
 
-        print "What else do you want to order?"
-        item = gets.chomp
+    print "#{name} wants to order: "
+    order = gets.chomp
 
-        puts "#{name} wants to order: #{item}"
+    orders[name] ||= []
+    orders[name].push(order)
 
-        #store the order in the hash
-        orders["name"] = "item"
+    print "Add another item to the order? (y/n)"
+    more = gets.chomp    
+    more.downcase
 
-        print "Add another item to the order?"
-        more = gets.chomp    
-        more.downcase
-    end
+    break if more != "y"
 
-#once done, print out all orders
-print orders
-#print print "All orders: " orders{name: item}
-#print "All orders: " orders.each{ |name, item| puts #{name}: #{item}}"
+end 
+
+    puts "All orders: "
+    puts orders
+
--- w03/d05/starter/roman/roman.js
+++ w03/d05/starter/roman/roman.js
@@ -1,41 +1,39 @@
 var number = prompt("What number do you want to convert to roman numerals?");
 
-function romanize(number);
+function romanize(number){
+    result = " ";
 
+    var matches = [
+        {numbers: 1, roman: "I"},
+        {numbers: 2, roman: "II"},
+        {numbers: 3, roman: "III"},
+        {numbers: 4, roman: "IV"},
+        {numbers: 5, roman: "V"},
+        {numbers: 6, roman: "VI"},
+        {numbers: 7, roman: "VII"},
+        {numbers: 8, roman: "VIII"},
+        {numbers: 9, roman: "IX"},
+        {numbers: 10, roman: "X"},
+        {numbers: 50, roman: "L"},
+        {numbers: 100, roman: "C"},
+        {numbers: 500, roman: "D"},
+        {numbers: 1000, roman: "M"},
+        {numbers: 40, roman: "XL"},
+        {numbers: 90, roman: "XC"},
+        {numbers: 400, roman: "CD"},
+        {numbers: 900, roman: "CM"}
+    ];
 
-function romanize(){
-
-    var roman;
-    var i;
-
-    var matches = {
-    1: "I",
-    2: "II",
-    3: "III",
-    4: "IV",
-    5: "V",
-    6: "VI",
-    7: "VII",
-    8: "VIII",
-    9: "IX",
-    10: "X",
-    50: "L",
-    100: "C",
-    500: "D",
-    1000: "M",
-    40: "XL",
-    90: "XC",
-    400: "CD",
-    900: "CM"
-    }
-
-//    for (matches[i]) {
-        for (i in matches) {
-        while (number >= matches[i]) {
-            roman += i;
-            number -= matches[i];
+    for (var i = 0; i < matches.length; i++) {
+        matches = matches[i];
+        
+        while (number >= matches.numbers) {
+            result += matches.roman;
+            number -= matches.numbers;
         }
+    
     }
-    return roman;
-    console.log(roman);
+    
+    console.log(result);
+    return result;
 }
